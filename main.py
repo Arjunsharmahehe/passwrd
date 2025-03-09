dc3e2944e343a2289018e8661f2fa6794f269261
Arjunsharmahehe <whocaresaboutarjun@gmail.com>
2025-03-09 13:19:25 +0530

    init

---
+++ main.py
@@ -0,0 +1,77 @@
+import json
+import os
+import hashlib
+DATA_FILE = 'passwords.json'
+
+class PasswordManager:
+    def __init__(self, seed):
+        self.key = hashlib.sha256(seed.encode()).digest()
+        self.passwords = self.load_password()
+
+    def load_password(self):
+        if os.path.exists(DATA_FILE):
+            with open(DATA_FILE, 'r') as f:
+                return json.load(f)
+        return {}
+    
+    def add_password(self, application, user, password):
+        if application not in self.passwords:
+            self.passwords[application] = []
+        d = {}
+        d["username"] = user
+        d["password"] = password
+        self.passwords[application].append(d)
+
+        with open(DATA_FILE, 'w') as f:
+            json.dump(self.passwords, f, indent=4)
+        return
+    
+    def get_password(self, application, user):
+        if application not in self.passwords:
+            print(f"No password for {application} is saved")
+            return
+        for object in self.passwords[application]:
+            if object["username"] == user:
+                print(f"Username: {object["username"]} \nPassword: {object["password"]}")
+                return
+        else:
+            print(f"No passwords for {user} has been saved in {application}")
+            return
+    
+    def update_password(self, application, user, newpassword):
+        if application not in self.passwords:
+            print(f"No passwords have been saved for {application}")
+            return
+        for index in range(len(self.passwords[application])):
+            if self.passwords[application][index]["username"] == user:
+                self.passwords[application][index]["password"] = newpassword
+                print("\nPassword updated successfully!!\n")
+                break
+        else:
+            print(f"No password for {user} has been saved in {application}")
+
+        with open(DATA_FILE, 'w') as f:
+            json.dump(self.passwords, f, indent=4)
+        return
+
+    
+pm = PasswordManager("arjun")
+print(pm.key)
+
+
+
+# d = {
+#     "instagram": [
+#         {
+#             "username": "arjunsharmahehe",
+#             "password": "arjunwwe6969"
+#         },
+#         {
+#             "username": "anushka",
+#             "password": "btch"
+#         }
+#     ]
+# }
+
+# with open(DATA_FILE, 'r+') as f:
+#     data = json.dump(d, f, indent=4)
